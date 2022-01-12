8fcf50461b634bb157b3b9c64b76c093222def8a
Jeneesh Kurian <jkurian@vividsolutions.com>
2022-01-12 12:31:15 -0800

    Updating changes to WFDM-Document_indexing Upsert documents into elastic search Read properies from file Get credentails from AWS Secret Manager Changes to extract data from SQS payload

--- wfdm-file-index-service/src/main/java/ca/bc/gov/nrs/wfdm/wfdm_opensearch_indexing/OpenSearchRESTClient.java
+++ wfdm-file-index-service/src/main/java/ca/bc/gov/nrs/wfdm/wfdm_opensearch_indexing/OpenSearchRESTClient.java
@@ -1,21 +1,30 @@
 package ca.bc.gov.nrs.wfdm.wfdm_opensearch_indexing;
 
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequestInterceptor;
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.json.JSONObject;
 
 import com.amazonaws.auth.AWS4Signer;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * OpenSeachRESTClient provides access to the OpenSearch Restful API. This is used
@@ -26,9 +35,10 @@ public class OpenSearchRESTClient {
   // should likely be moved into a config file...
   private static String serviceName = "es";
   private static String region = "ca-central-1";
-  private static String domainEndpoint = "add URL here";
-  private static String index = "wfdm-opensearch-index";
-  private static String id = String.valueOf(System.currentTimeMillis());
+  private static String domainEndpoint = "";
+  private static String indexName = "";
+  static RestHighLevelClient restClient;
+
 
   static final AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
 
@@ -38,34 +48,97 @@ public class OpenSearchRESTClient {
    * @param content
    * @param fileName
    * @return
+   * 
    * @throws IOException
    */
-  public IndexResponse addIndex(String content, String fileName, JSONObject fileDetails) throws IOException {
-    RestHighLevelClient searchClient = searchClient(serviceName, region);
-    String type = "_doc";
+	public IndexResponse addIndex(String content, String fileName, JSONObject fileDetails) throws IOException {
+		restClient = searchClient(serviceName, region);
 
-    Map<String, Object> document = new HashMap<>();
-    document.put("key", fileName);
-    document.put("text", content);
-    // attach the metadata and security
-    document.put("metadata", fileDetails.getJSONArray("metadata").toString());
-    document.put("security", fileDetails.getJSONArray("security").toString());
+		String type = "_doc";
 
-    // Form the indexing request, send it, and print the response
-    IndexRequest request = new IndexRequest(index, type, id).source(document);
+		Map<String, Object> document = new HashMap<>();
+		document.put("key", fileName);
+		document.put("text", content);
+		document.put("fileName", fileName);
+		document.put("metadata", fileDetails.getJSONArray("metadata").toString());
+		document.put("security", fileDetails.getJSONArray("security").toString());
+		String id = fileDetails.getString("fileId");
 
-    return searchClient.index(request, RequestOptions.DEFAULT);
-  }
+		String json;
+		ObjectMapper mapper = new ObjectMapper();
 
-  // Adds the interceptor to the OpenSearch REST client
-  private static RestHighLevelClient searchClient(String serviceName, String region) {
-    AWS4Signer signer = new AWS4Signer();
-    signer.setServiceName(serviceName);
-    signer.setRegionName(region);
+		try {
+			json = mapper.writeValueAsString(document);
+		} catch (JsonProcessingException e) {
+			throw new ElasticsearchException("JSON?????", e);
+		}
 
-    HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer,
-        credentialsProvider);
-    return new RestHighLevelClient(RestClient.builder(HttpHost.create(domainEndpoint))
-        .setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)));
-  }
+		IndexRequest indexRequest = new IndexRequest(indexName, type, id);
+		Set<String> keys1 = document.keySet();
+		XContentBuilder builder1 = jsonBuilder().startObject();
+		for (String key : keys1) {
+			builder1.field(key, document.get(key));
+		}
+
+		builder1.endObject();
+		indexRequest.source(builder1);
+
+		Set<String> keys = document.keySet();
+		XContentBuilder builder = jsonBuilder().startObject();
+		for (String key : keys) {
+			builder.field(key, document.get(key));
+		}
+		builder.endObject();
+
+		UpdateRequest updateRequest = new UpdateRequest();
+		updateRequest.index(indexName);
+		updateRequest.type(type);
+		updateRequest.id(id);
+		updateRequest.doc(builder);
+		updateRequest.upsert(indexRequest);
+
+		// Form the indexing request, send it, and print the response
+		IndexRequest createRequest = new IndexRequest(indexName, type, id).source(document);
+
+		IndexResponse response = null;
+		try {
+			response = restClient.index(createRequest, RequestOptions.DEFAULT);
+			System.out.println("Response from OpenSearch:" + response.toString());
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Close the rest connection
+		if (restClient != null) {
+			try {
+				System.out.println("closing rest connection");
+				restClient.close();
+				restClient = null;
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return response;
+	}
+  
+  
+//Adds the intercepter to the OpenSearch REST client
+	public  RestHighLevelClient searchClient(String serviceName, String region) {
+		AWS4Signer signer = new AWS4Signer();
+		signer.setServiceName(serviceName);
+		signer.setRegionName(region);
+		HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer,
+				credentialsProvider);
+		RestClientBuilder builder = RestClient
+				.builder(HttpHost.create(domainEndpoint))
+				.setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor));
+		
+		restClient = new RestHighLevelClient(builder);
+	
+		return restClient;
+	}
 }
+
