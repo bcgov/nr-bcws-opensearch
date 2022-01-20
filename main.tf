6a13965b67059a35a50cea4dc455b9ae056304c8
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-01-20 14:20:05 -0800

    Update main.tf
    Changes attempting to address syntax error

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-# The configuration for the `remote` backend.
 terraform {
     backend "remote" {
-        # The name of your Terraform Cloud organization.
         organization = "vivid-solutions"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
             name = "nr-bcws-opensearch"
         }
@@ -12,7 +8,4 @@ terraform {
 }
 
 resource "null_resource" "example" {
-    triggers = {
-        value = "A example resource that does nothing!"
-    }
 }
