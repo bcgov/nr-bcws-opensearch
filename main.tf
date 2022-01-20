c4a18bf371072b99a51b9aa7623a85da594c5179
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-01-20 15:33:44 -0800

    Added initial variables and aws resource to main.tf

--- main.tf
+++ main.tf
@@ -20,5 +20,72 @@ terraform {
   }
 }
 
-resource "null_resource" "example_1" {
+//VARIABLES USED ACROSS ALL RESOURCES
+variable "application" {
+    type="string"
+    description="name of application"
+    default="WFDM"
+}
+
+variable "customer" {
+    type="string"
+    description="name of customer"
+    default=WildFireOne
+}
+
+variable "tool" {
+    type="string"
+    description="name of aws tool being used"
+    default="opensearch"
+}
+
+variable "region" {
+  type = string
+  description = "AWS Region, where to deploy cluster."
+  default = "ca-central-1"
+}
+
+variable "custom_endpoint_url" {
+    type=string
+    description="URL matching custom endpoint cert"
+    default="bcwildfireservices.com"
+}
+
+variable "env" {
+  type = string
+  description = "Suffix appended to all managed resource names to indicate their environment"
+  default = null
+}
+
+variable "pr" {
+  type = string
+  description = "Suffix appended to all managed resource names"
+  default = "0"
+}
+
+variable "suffix" {
+  type = string
+  description = "Suffix appended to all managed resource names"
+  default = null
+}
+
+//CREATE THE VPC AND SUBNETS
+//Main VPC
+variable "vpc_cidr_block" {
+  type=string
+  description="CIDR block to be used by vpc"
+  default="10.0.0.0/16"
+}
+
+resource "aws_vpc" "main_vpc" {
+    cidr_block = "${var.vpc_cidr_block}"
+    operating_regions {
+        region_name=var.region
+    }
+    tags {
+        Name = "${var.application}-vpc-${var.env}"
+        Application = var.application
+        Customer = var.customer
+        Environment = var.env
+    }
 }
