809bce003c71c4028f5f9e931495194cf99180b8
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-01-20 17:05:08 -0800

    Moved variable declarations back into main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,61 @@ terraform {
   }
 }
 //Variable declarations (values in terraform.auto.tfvars)
-variable "application" {}
-variable "env" {}
-variable "customer" {}
-variable "vpc_cidr_block" {}
-variable "region" {}
+#VARIABLES USED ACROSS ALL RESOURCES
+variable "application" {
+  type        = string
+  description = "name of application"
+  default     = "WFDM"
+}
+
+variable "customer" {
+  type        = string
+  description = "name of customer"
+  default     = WildFireOne
+}
+
+variable "tool" {
+  type        = string
+  description = "name of aws tool being used"
+  default     = "opensearch"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS Region, where to deploy cluster."
+  default     = "ca-central-1"
+}
+
+variable "custom_endpoint_url" {
+  type        = string
+  description = "URL matching custom endpoint cert"
+  default     = "bcwildfireservices.com"
+}
+
+variable "env" {
+  type        = string
+  description = "Suffix appended to all managed resource names to indicate their environment"
+  default     = null
+}
+
+variable "pr" {
+  type        = string
+  description = "Suffix appended to all managed resource names"
+  default     = "0"
+}
+
+variable "suffix" {
+  type        = string
+  description = "Suffix appended to all managed resource names"
+  default     = null
+}
+
+#VPC-RELATED VARIABLES
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block to be used by vpc"
+  default     = "10.0.0.0/16"
+}
 
 
 //CREATE THE VPC AND SUBNETS
