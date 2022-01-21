1c33df209a9bbafb1701c671f385739f65ecab67
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-01-20 17:21:19 -0800

    Specified region, removed unneeded resources

--- main.tf
+++ main.tf
@@ -73,33 +73,17 @@ variable "vpc_cidr_block" {
 }
 
 
+provider "aws" {
+  region = var.region
+}
+
+
+
 //CREATE THE VPC AND SUBNETS
 //Main VPC
 
-resource "aws_vpc_ipam" "test" {
-  operating_regions {
-    region_name = var.region
-  }
-}
-
-resource "aws_vpc_ipam_pool" "test" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
-  locale         = var.region
-}
-
-resource "aws_vpc_ipam_pool_cidr" "test" {
-  ipam_pool_id = aws_vpc_ipam_pool.test.id
-  cidr         = var.vpc_cidr_block
-}
-
 resource "aws_vpc" "main_vpc" {
-  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.test.id
-  ipv4_netmask_length = 16
-  depends_on = [
-    aws_vpc_ipam_pool_cidr.test
-  ]
-  instance_tenancy = "default"
+  cidr_block = var.vpc_cidr_block
 
   tags = {
     Name        = "${var.application}-vpc-${var.env}"
