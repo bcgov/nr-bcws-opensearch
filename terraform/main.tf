121c301f1bf09c13f7ef9eb92b4b381046a1c6f4
vivid-cpreston <97257824+vivid-cpreston@users.noreply.github.com>
2022-02-11 18:00:25 -0800

    Added demo resource to fix broken dependency

--- terraform/main.tf
+++ terraform/main.tf
@@ -1009,6 +1009,12 @@ EOF
   ]
 }
 
+resource "aws_api_gateway_resource" "sqs-api-gateway-resource" {
+  rest_api_id = aws_api_gateway_rest_api.sqs-api-gateway.id
+  parent_id   = aws_api_gateway_rest_api.sqs-api-gateway.root_resource_id
+  path_part   = "mydemoresource"
+}
+
 resource "aws_api_gateway_method_response" "http200" {
   rest_api_id = aws_api_gateway_rest_api.sqs-api-gateway.id
   resource_id = aws_api_gateway_rest_api.sqs-api-gateway.root_resource_id
