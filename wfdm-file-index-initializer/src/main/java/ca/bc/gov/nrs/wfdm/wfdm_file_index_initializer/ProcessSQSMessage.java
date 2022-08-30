5d62962f2ed5041b57eddb321a58f3dbc9d0ef42
cbergin <cbergin@vividsolutions.com>
2022-08-30 16:05:29 -0700

    RevertingToDirectoryTest

--- wfdm-file-index-initializer/src/main/java/ca/bc/gov/nrs/wfdm/wfdm_file_index_initializer/ProcessSQSMessage.java
+++ wfdm-file-index-initializer/src/main/java/ca/bc/gov/nrs/wfdm/wfdm_file_index_initializer/ProcessSQSMessage.java
@@ -97,7 +97,7 @@ public class ProcessSQSMessage implements RequestHandler<SQSEvent, SQSBatchRespo
         JSONObject fileDetailsJson = new JSONObject(fileInfo);
         String mimeType = fileDetailsJson.get("mimeType").toString();
 
-        if (mimeType.equals("null")) { mimeType = "application/vnd.bcgov-wfdm.folder";}
+        if (mimeType.equals("null")) { mimeType = "DIRECTORY";}
 
         // Check the event type. If this is a BYTES event, write the bytes
         // otherwise, handle meta only and skip clam scan.
