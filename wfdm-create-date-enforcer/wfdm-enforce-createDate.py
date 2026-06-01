0bce61c4ea80c3d68ada1c5c19600b5d1bdf67a5
Sean Sylver <ssylver@vividsolutions.com>
2026-06-01 15:05:21 -0700

    Enable top-level folder functionality for wfdm-enfore-createDate.py

--- wfdm-create-date-enforcer/wfdm-enforce-createDate.py
+++ wfdm-create-date-enforcer/wfdm-enforce-createDate.py
@@ -21,6 +21,7 @@ doc_root = '?parentId='
 row_count = row_count = os.getenv('QUERY_ROW_COUNT')
 token = None
 token_expiry = 0
+top_level_folder_id = os.getenv('TOP_LEVEL_FOLDER_ID')
 
 def createDate_formatter(unformatted_date):
   return unformatted_date.replace("T", " ").split(".")[0]
@@ -189,21 +190,30 @@ def enforce_createDate(document_id, page, row_count):
 
 # Step #2, now that we have a nice shiny new token, lets go fetch from WFDM why not
 # First though, we need to know our Root ID
-print('Fetching The WFDM Root Document...')
-try:
-  wfdm_root_response = api_request("GET", doc_endpoint + wfdm_root)
-  if wfdm_root_response.status_code != 200:
-    print(wfdm_root_response)
-    sys.exit("Failed to fetch from WFDM. Response code was: " + str(wfdm_root_response.status_code))
-    
-except requests.exceptions.RequestException as e:
-    sys.exit(f"Root fetch failed: {e}")
+# Root ID can be provided to run the script against a certain parent folder e.g. WFDM, WFFIN, WFHR, WFIM, WFPREV, WFRM, WFWX
+# Fallback to running against every file if Root ID is not provided
+if top_level_folder_id:
+    verify_response = api_request("GET", docs_endpoint + '/' + top_level_folder_id)
+    if verify_response.status_code != 200:
+        sys.exit(f"TOP_LEVEL_FOLDER_ID {top_level_folder_id} not found: {verify_response.status_code}")
+    print(f'Starting from folder {top_level_folder_id}')
+    enforce_createDate(top_level_folder_id, 1, row_count)
+else:
+    print('Fetching The WFDM Root Document...')
+    try:
+      wfdm_root_response = api_request("GET", doc_endpoint + wfdm_root)
+      if wfdm_root_response.status_code != 200:
+        print(wfdm_root_response)
+        sys.exit("Failed to fetch from WFDM. Response code was: " + str(wfdm_root_response.status_code))
 
-# Pull out the fileId, this is our parent for WFDM
-root_id = wfdm_root_response.json()['fileId']
-del wfdm_root_response
-# start the metadata update
-print('... Done! Starting createDate append from root document ' + str(root_id))
-#enforce_createDate(root_id, 1, row_count)
-enforce_createDate(root_id, 1, row_count)
+    except requests.exceptions.RequestException as e:
+        sys.exit(f"Root fetch failed: {e}")
+
+    # Pull out the fileId, this is our parent for WFDM
+    root_id = wfdm_root_response.json()['fileId']
+    del wfdm_root_response
+    # start the metadata update
+    print('... Done! Starting createDate append from root document ' + str(root_id))
+    #enforce_createDate(root_id, 1, row_count)
+    enforce_createDate(root_id, 1, row_count)
 
