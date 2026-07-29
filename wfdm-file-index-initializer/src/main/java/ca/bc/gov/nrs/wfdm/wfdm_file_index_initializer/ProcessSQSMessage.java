7eb4be9204c10f33dedcc4d0e6c6ce7216a29e28
Cody Bergin <cbergin@vividsolutions.com>
2026-07-29 16:23:47 -0700

    WFDM-1186 Reduce complexity for process sqs messages on initializer index

--- wfdm-file-index-initializer/src/main/java/ca/bc/gov/nrs/wfdm/wfdm_file_index_initializer/ProcessSQSMessage.java
+++ wfdm-file-index-initializer/src/main/java/ca/bc/gov/nrs/wfdm/wfdm_file_index_initializer/ProcessSQSMessage.java
@@ -15,7 +15,6 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClient;
 import com.amazonaws.services.lambda.model.InvokeRequest;
-import com.amazonaws.services.lambda.model.InvokeResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -152,7 +151,6 @@ public class ProcessSQSMessage implements RequestHandler<SQSEvent, SQSBatchRespo
     logger.log("\nInfo: delay running file index initializer lambda for 5 min to avoid file update racing condition");
     delayProcessing();
    
-
     // Iterate the available messages
     for (SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
       try {
@@ -174,28 +172,12 @@ public class ProcessSQSMessage implements RequestHandler<SQSEvent, SQSBatchRespo
 
         String versionNumber = messageDetails.getString("fileVersionNumber");
 
-        String wfdmSecretName = getSecretManagerName().trim();
-        String secret = RetrieveSecret.RetrieveSecretValue(wfdmSecretName);
-        String[] secretCD = StringUtils.substringsBetween(secret, "\"", "\"");
-        String CLIENT_ID = secretCD[0];
-        String PASSWORD = secretCD[1];
+        String wfdmToken = retrieveWFDMToken(logger);
 
-        logger.log("retrieved secret and client_ID and PASSWORD");
+        HttpResponse<String> fileResponse = getFileResponse(wfdmToken, fileId);
 
-        String wfdmToken = GetFileFromWFDMAPI.getAccessToken(CLIENT_ID, PASSWORD);
-        if (wfdmToken == null)
-          throw new Exception("Could not authorize access for WFDM");
-
-        HttpResponse<String> fileResponse = GetFileFromWFDMAPI.getFileInformation(wfdmToken, fileId);
-
-        
-
-        if (fileResponse == null) {
-          throw new Exception("File not found!");
-        } else {
-          String fileInfo = fileResponse.getBody();
-          String etag = fileResponse.getHeaders().getFirst("ETag");
-
+        String fileInfo = fileResponse.getBody();
+        String etag = fileResponse.getHeaders().getFirst("ETag");
 
         JSONObject fileDetailsJson = new JSONObject(fileInfo);
 
@@ -207,79 +189,13 @@ public class ProcessSQSMessage implements RequestHandler<SQSEvent, SQSBatchRespo
 
         boolean isHeicOrHeif = isHeicOrHeif(fileExtension);
 
-        if (shouldAbortImageConversion( fileTooLargeToConvert, isHeicOrHeif)) {
-          GetFileFromWFDMAPI.setImageConversionMetadata(wfdmToken, fileId, versionNumber,
-          fileDetailsJson, "Image Conversion aborted due to file size", etag);
-        }
-        // if a file has a heic or heif mimetype it needs to be converted by the image
-        // conversion lambda rather than processed
-        if (shouldInvokeImageConverter(fileTooLargeToConvert, isHeicOrHeif)) {
-          logger.log("\nInfo: File with mimeType of " + mimeType + " calling image conversion lambda");
-          AWSLambda client = createLambdaClient();
-          InvokeRequest request = new InvokeRequest();
-          request.withFunctionName(getImageConverterLambdaName().trim()).withPayload(fileDetailsJson.toString());
-          InvokeResult invoke = client.invoke(request);
-        } else {
+        if (!processImageConversion(fileTooLargeToConvert, isHeicOrHeif, wfdmToken,
+          fileId, versionNumber, fileDetailsJson, etag, mimeType, logger)) {
 
-          // Check the event type. If this is a BYTES event, write the bytes
-          // otherwise, handle meta only and skip clam scan.
-          if (eventType.equalsIgnoreCase("bytes")) {
-            logger.log("\nInfo: File found on WFDM: " + fileInfo);
-            // Update Virus scan metadata
-            // Note, current user likely lacks access to update metadata so we'll need to update webade
-            boolean metaAdded = GetFileFromWFDMAPI.setVirusScanMetadata(wfdmToken, fileId, versionNumber,
-                fileDetailsJson, etag);
-            if (!metaAdded) {
-              // We failed to apply the metadata regarding the virus scan status...
-              // Should we continue to process the data from this point, or just choke?
-              logger.log("\nERROR: Failed to add metadata to file resource");
-            }
-
-            AmazonS3 s3client = createS3Client();
-
-            Bucket clamavBucket = null;
-            String bucketName = getClamAvBucketName().trim();
-
-            List<Bucket> buckets = s3client.listBuckets();
-            for (Bucket bucket : buckets) {
-              if (bucket.getName().equalsIgnoreCase(bucketName)) {
-                clamavBucket = bucket;
-              }
-            }
-
-            if (clamavBucket == null) {
-              throw new Exception("S3 Bucket " + bucketName + " does not exist.");
-            }
-
-            BufferedInputStream stream = GetFileFromWFDMAPI.getFileStream(wfdmToken, fileId, versionNumber);
-
-            ObjectMetadata meta = new ObjectMetadata();
-            meta.setContentType(mimeType);
-            meta.setContentLength(Long.parseLong(fileDetailsJson.get(FILE_SIZE).toString()));
-            meta.addUserMetadata("title", fileId + "-" + versionNumber);
-            logger.log("putting into s3 bucket");
-            s3client.putObject(new PutObjectRequest(clamavBucket.getName(),
-                fileDetailsJson.get("fileId").toString() + "-" + versionNumber, stream, meta));
-          }
-          //handling to allow folders to be added to opensearch bypassing the clamAv scan and sending them directly to the file index service
-          else if (eventType.equalsIgnoreCase("meta") && (fileDetailsJson.get(MIME_TYPE).toString().equals("null"))) {
-            AWSLambda client = createLambdaClient();
-            InvokeRequest request = new InvokeRequest();
-            request.withFunctionName(getIndexingLambdaName().trim())
-                .withPayload(fileDetailsJson.toString());
-            InvokeResult invoke = client.invoke(request);
-
-          } else {
-            // Meta only update, so fire a message to the Indexer Lambda
-            logger.log("Calling lambda name: " + getIndexingLambdaName().trim() + " lambda: "
-                + messageBody);
-            AWSLambda client = createLambdaClient();
-            InvokeRequest request = new InvokeRequest();
-            request.withFunctionName(getIndexingLambdaName().trim()).withPayload(messageBody);
-            InvokeResult invoke = client.invoke(request);
-          }
-        }
-        }
+        processEventType(eventType, wfdmToken, fileId, versionNumber, fileInfo,
+            fileDetailsJson, etag, mimeType, messageBody, logger);
+      }
+        
       } catch (UnirestException | TransformerConfigurationException | SAXException e) {
         logger.log("logged exception" + e);
         logger.log("\nError: Failure to recieve file from WFDM: " + e.getLocalizedMessage());
@@ -296,5 +212,162 @@ public class ProcessSQSMessage implements RequestHandler<SQSEvent, SQSBatchRespo
     return new SQSBatchResponse(batchItemFailures);
   }
 
+  private String retrieveWFDMToken(LambdaLogger logger) throws Exception {
+    String wfdmSecretName = getSecretManagerName().trim();
+    String secret = RetrieveSecret.RetrieveSecretValue(wfdmSecretName);
+    String[] secretCD = StringUtils.substringsBetween(secret, "\"", "\"");
+
+    String clientId = secretCD[0];
+    String password = secretCD[1];
+
+    logger.log("retrieved secret and client_ID and PASSWORD");
+
+    String wfdmToken = GetFileFromWFDMAPI.getAccessToken(clientId, password);
+
+    if (wfdmToken == null) {
+      throw new Exception("Could not authorize access for WFDM");
+    }
+
+    return wfdmToken;
+  }
+
+  private void updateVirusScanMetadata(String wfdmToken, String fileId, String versionNumber,
+      JSONObject fileDetailsJson, String etag, LambdaLogger logger) throws Exception {
+
+    boolean metaAdded = GetFileFromWFDMAPI.setVirusScanMetadata( wfdmToken, fileId, versionNumber,
+        fileDetailsJson, etag);
+
+    if (!metaAdded) {
+      logger.log("\nERROR: Failed to add metadata to file resource");
+    }
+  }
+
+  private Bucket getClamAvBucket( AmazonS3 s3client) throws Exception {
+
+    String bucketName = getClamAvBucketName().trim();
+
+    for (Bucket bucket : s3client.listBuckets()) {
+      if (bucket.getName().equalsIgnoreCase(bucketName)) {
+        return bucket;
+      }
+    }
+
+    throw new Exception( "S3 Bucket " + bucketName + " does not exist.");
+  }
+
+  private void uploadFileToClamAvBucket(AmazonS3 s3client, Bucket clamavBucket, String wfdmToken, String fileId,
+    String versionNumber, String mimeType, JSONObject fileDetailsJson, LambdaLogger logger)throws Exception {
+
+    BufferedInputStream stream = GetFileFromWFDMAPI.getFileStream(wfdmToken, fileId, versionNumber);
+
+    ObjectMetadata meta = new ObjectMetadata();
+    meta.setContentType(mimeType);
+    meta.setContentLength(Long.parseLong(fileDetailsJson.get(FILE_SIZE).toString()));
+    meta.addUserMetadata("title", fileId + "-" + versionNumber);
+
+    logger.log("putting into s3 bucket");
+
+    s3client.putObject(new PutObjectRequest(clamavBucket.getName(),
+        fileDetailsJson.get("fileId").toString() + "-" + versionNumber,
+        stream, meta));
+  }
+
+  private void invokeImageConverter(JSONObject fileDetailsJson, String mimeType, LambdaLogger logger) {
+    logger.log("\nInfo: File with mimeType of " + mimeType + " calling image conversion lambda");
+
+    AWSLambda client = createLambdaClient();
+    InvokeRequest request = new InvokeRequest();
+
+    request.withFunctionName(getImageConverterLambdaName().trim()).withPayload(fileDetailsJson.toString());
+
+    client.invoke(request);
+  }
+
+  private void invokeIndexerWithFileDetails(JSONObject fileDetailsJson) {
+    AWSLambda client = createLambdaClient();
+    InvokeRequest request = new InvokeRequest();
+
+    request.withFunctionName(getIndexingLambdaName().trim())
+        .withPayload(fileDetailsJson.toString());
+
+    client.invoke(request);
+  }
+
+  private void invokeIndexer(String messageBody, LambdaLogger logger) {
+    logger.log("Calling lambda name: " + getIndexingLambdaName().trim() + " lambda: " + messageBody);
+
+    AWSLambda client = createLambdaClient();
+    InvokeRequest request = new InvokeRequest();
+
+    request.withFunctionName(getIndexingLambdaName().trim())
+      .withPayload(messageBody);
+
+    client.invoke(request);
+  }
+
+  private void processBytesEvent(String wfdmToken, String fileId, String versionNumber, String fileInfo,
+    JSONObject fileDetailsJson, String etag, String mimeType,
+    LambdaLogger logger) throws Exception {
+
+    logger.log("\nInfo: File found on WFDM: " + fileInfo);
+
+    updateVirusScanMetadata(wfdmToken, fileId, versionNumber, fileDetailsJson, etag, logger);
+
+    AmazonS3 s3client = createS3Client();
+
+    Bucket clamavBucket = getClamAvBucket(s3client);
+
+    uploadFileToClamAvBucket(s3client, clamavBucket, wfdmToken, fileId,
+        versionNumber, mimeType, fileDetailsJson, logger);
+  }
+
+  private void processEventType(String eventType, String wfdmToken, String fileId,
+      String versionNumber, String fileInfo, JSONObject fileDetailsJson,
+      String etag, String mimeType, String messageBody,
+      LambdaLogger logger) throws Exception {
+
+    if (eventType.equalsIgnoreCase("bytes")) {
+      processBytesEvent(wfdmToken, fileId, versionNumber, fileInfo,
+          fileDetailsJson, etag, mimeType, logger);
+
+    } else if (eventType.equalsIgnoreCase("meta")
+        && fileDetailsJson.get(MIME_TYPE).toString().equals("null")) {
+
+      invokeIndexerWithFileDetails(fileDetailsJson);
+
+    } else {
+      invokeIndexer(messageBody, logger);
+    }
+  }
+
+  private HttpResponse<String> getFileResponse(String wfdmToken, String fileId) throws Exception {
+
+    HttpResponse<String> fileResponse = GetFileFromWFDMAPI.getFileInformation(wfdmToken, fileId);
+
+    if (fileResponse == null) {
+      throw new Exception("File not found!");
+    }
+
+    return fileResponse;
+  }
+
+  private boolean processImageConversion(boolean fileTooLargeToConvert,
+      boolean isHeicOrHeif, String wfdmToken, String fileId,
+      String versionNumber, JSONObject fileDetailsJson,
+      String etag, String mimeType, LambdaLogger logger) throws Exception {
+
+    if (shouldAbortImageConversion(fileTooLargeToConvert, isHeicOrHeif)) {
+      GetFileFromWFDMAPI.setImageConversionMetadata(wfdmToken, fileId,
+          versionNumber, fileDetailsJson,
+          "Image Conversion aborted due to file size", etag);
+    }
+
+    if (shouldInvokeImageConverter(fileTooLargeToConvert, isHeicOrHeif)) {
+      invokeImageConverter(fileDetailsJson, mimeType, logger);
+      return true;
+    }
+
+    return false;
+  }
 
 }
